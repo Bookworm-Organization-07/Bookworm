c327c7313c1e0f9e9449ab0a13a7d3bedec8de40
NishkalSB <nishkalbhooshetty14@gmail.com>
2026-07-27 10:39:11 +0530

    Set up Spring Boot backend project structure

---
+++ backend-java/src/test/java/com/bookworm/backend/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookworm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
