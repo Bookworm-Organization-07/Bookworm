9597d228786c290f302072e7309c067a903b54af
ChinmayNerkar <149101924+ChinmayNerkar@users.noreply.github.com>
2026-08-24 15:58:18 +0530

    Add files via upload
    UserRepository

---
+++ backend/src/main/java/com/example/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.Repository;
+
+import com.example.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUserEmail(String userEmail);
+
+    boolean existsByUserEmail(String userEmail);
+}
