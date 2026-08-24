f95e49328b5f6ebe290f2ae493f32dd3964f6dad
Aman175099 <amannema71@gmail.com>
2026-08-24 19:16:29 +0530

    Add Language controller

---
+++ backend/src/main/java/com/example/Controller/LanguageController.java
@@ -0,0 +1,25 @@
+package com.example.Controller;
+
+import com.example.Repository.LanguageRepository;
+import com.example.models.Language;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/languages")
+public class LanguageController {
+
+    private final LanguageRepository languageRepository;
+
+    public LanguageController(LanguageRepository languageRepository) {
+        this.languageRepository = languageRepository;
+    }
+
+    @GetMapping
+    public List<Language> getAllLanguages() {
+        return languageRepository.findAll();
+    }
+}
